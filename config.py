7a8c61bd8c1756dc59b2a0097e7965858ba20ba1
primebaby <primebaby11220@gmail.com>
2025-11-04 13:26:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,4 +14,4 @@ DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "")
 
 IS_FSUB = os.environ.get("IS_FSUB", "True").lower() == "true"  # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-100******** -100*********").split())) # Add Multiple channel ids
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "").split())) # Add Multiple channel ids
